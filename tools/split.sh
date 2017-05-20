3965e964e369d1585552df973c56d7d65bb94335
Simon Bennett <simon@bennett.im>
2017-05-20 09:38:35 +0100

    [Split] ignore tags

--- tools/split.sh
+++ tools/split.sh
@@ -1,10 +1,10 @@
 git subsplit init git@github.com:SmoothPhp/CQRS-ES-Framework.git
 
-git subsplit publish --heads="master 1.0" src/CommandBus:git@github.com:SmoothPhp/CommandBus.git
-git subsplit publish --heads="master 1.0" src/Contracts:git@github.com:SmoothPhp/Contracts.git
-git subsplit publish --heads="master 1.0" src/Domain:git@github.com:SmoothPhp/Domain.git
-git subsplit publish --heads="master 1.0" src/EventBus:git@github.com:SmoothPhp/EventBus.git
-git subsplit publish --heads="master 1.0" src/EventDispatcher:git@github.com:SmoothPhp/EventDispatcher.git
-git subsplit publish --heads="master 1.0" src/EventSourcing:git@github.com:SmoothPhp/EventSourcing.git
-git subsplit publish --heads="master 1.0" src/Serialization:git@github.com:SmoothPhp/Serialization.git
-git subsplit publish --heads="master 1.0" src/EventStore:git@github.com:SmoothPhp/EventStore.git
+git subsplit publish --heads="master" src/CommandBus:git@github.com:SmoothPhp/CommandBus.git
+git subsplit publish --heads="master" src/Contracts:git@github.com:SmoothPhp/Contracts.git
+git subsplit publish --heads="master" src/Domain:git@github.com:SmoothPhp/Domain.git
+git subsplit publish --heads="master" src/EventBus:git@github.com:SmoothPhp/EventBus.git
+git subsplit publish --heads="master" src/EventDispatcher:git@github.com:SmoothPhp/EventDispatcher.git
+git subsplit publish --heads="master" src/EventSourcing:git@github.com:SmoothPhp/EventSourcing.git
+git subsplit publish --heads="master" src/Serialization:git@github.com:SmoothPhp/Serialization.git
+git subsplit publish --heads="master" src/EventStore:git@github.com:SmoothPhp/EventStore.git
